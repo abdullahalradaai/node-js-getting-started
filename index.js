19d490e6bd4ecb542c011484951a44c78976e23c
abdullah <aeqab77@gmail.com>
2020-06-26 01:18:54 +0300

    Add cool face API

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const cool = require('cool-ascii-faces');
 const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+const mongoose=require('mongoose');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -10,3 +11,18 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', (req, res) => res.send(cool()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+
+mongoose.connect(
+  'mongodb+srv://zajelsecurity:777151565@realmcluster-taahg.mongodb.net/test?retryWrites=true&w=majority'
+,{
+  dbName:'zajeldb',
+  user:'zajelsecurity',
+  pass:'777151565',
+  useNewUrlParser:true,
+  useUnifiedTopology:true}
+)
+.then(()=>{
+  console.log('connected ....')
+});
